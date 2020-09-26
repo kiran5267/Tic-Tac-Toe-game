7b8c9f14dfcfce8bf5a499218474540b8c7300d7
kiran5267 <kiran567456@gmail.com>
2020-09-26 11:01:05 +0530

    UseCase 3

--- Tictactoe.java
+++ Tictactoe.java
@@ -1,8 +1,11 @@
 import java.util.*;
 class Tictactoe{
 	//UC 1
+	char[] board=new char[10];
    void initializeBoard(){
-		char[] board={' ',' ',' ',' ',' ',' ',' ',' ',' ',' '};
+		for(int i=0;i<10;i++){
+			board[i]=' ';
+		}
 	}
 	//UC 2
 	void chooseLetter(){
@@ -18,10 +21,21 @@ class Tictactoe{
 		}
 		System.out.println("User Choise = "+userChoice+" Cumputer Choice= "+comChoice);
 	}
+	//UC 3
+	void showBoard(){
+		System.out.println(board[1]+"|"+board[2]+"|"+board[3]);
+		System.out.println("-----");
+      System.out.println(board[4]+"|"+board[5]+"|"+board[6]);
+      System.out.println("-----");
+      System.out.println(board[7]+"|"+board[8]+"|"+board[9]);
+	}
+
+
    public static void main(String[] args){
-      Tictactoe obj=new Tictactoe();
-      obj.initializeBoard();
-      obj.chooseLetter();
+      Tictactoe obj1=new Tictactoe();
+      obj1.initializeBoard();
+      obj1.chooseLetter();
+		obj1.showBoard();
    }
 }
 
